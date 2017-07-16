5e27cc0d3c8f3b6fb9f1c959464da79f738c042c
codz30 <codylgeorge@live.com.au>
2017-07-16 20:56:20 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,10 @@
 var cheapSeatApp = angular.module('cheapSeatApp', []);
 
 cheapSeatApp.controller('DealListController', ['$scope', '$http', function($scope, $http) {
-    $scope.method = 'GET';
-    $scope.url = 'http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361';
-    $scope.code = null;
-    $scope.response = null;
-    $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-        then(function(response) {
-          $scope.status = response.status;
-          $scope.data = response.data;
-        }, function(response) {
-          $scope.data = response.data || 'Request failed';
-          $scope.status = response.status;
-          $scope.deals = xml2json($scope.data, " ");
-     });
+    $http.get('http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361')
+    .then(function(response) {
+        $scope.deals = xml2json(response.data, " ");
+    });
 }]);
 
 
