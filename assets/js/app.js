8670a096d982160e3bc4733e654d08c53920f8ee
codz30 <codylgeorge@live.com.au>
2017-07-16 22:33:29 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 var cheapSeatApp = angular.module('cheapSeatApp', []);
 
 cheapSeatApp.controller('DealListController', ['$scope', '$http', function($scope, $http) {
+    $scope.loading = true;
     $http.get('https://cors-anywhere.herokuapp.com/http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361')
     .then(function(response) {
 	var x2js = new X2JS();
 	var xml = response.data.replace(/&/g, "&amp;");   
         $scope.deals = x2js.xml_str2json(xml).deals.deal;
+    }).finally(function(){
+	 $scope.loading = false; 
     });
 }]);
