ada3cbb5dc710bfbdd024f621dd6f141848537f9
codz30 <codylgeorge@live.com.au>
2017-07-16 20:47:05 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,17 @@
 var cheapSeatApp = angular.module('cheapSeatApp', []);
 
-cheapSeatApp.controller('DealListController', ['$scope', function($scope) {
-  $scope.deals = [
-    {
-      title: 'Flights to Thailand from $282 Return on Scoot Airlines. Min 2 passengers.',
-      description: 'Scoot is having a sale on flights to Bangkok, Thailand. Travel in 25/Jul - 15/Nov. Minimum 2 passengers.',
-      url: 'http://iwantthatflight.com.au/d12467-Flights-to-Thailand-from-282-Return-on-Scoot-Airlines-Min-2-passengers.aspx?afid=2361',
-      image: 'http://iwantthatflight.com.au/pics/locations/BKK_640.jpg'
-    }, {
-      title: 'Flights to Japan from $649 Return on Japan Airlines',
-      description: 'Japan Airlines has reduced prices even further on flights to Tokyo. Travel in Oct - Nov/17. Flights are direct from Melbourne, other cities fly Qantas to Melbourne then onto Japan.',
-      url: 'http://iwantthatflight.com.au/d12468-Flights-to-Japan-from-649-Return-on-Japan-Airlines.aspx?afid=2361',
-      image: 'http://iwantthatflight.com.au/pics/locations/NRT_640.jpg'
-    }, {
-      title: 'Flights to Singapore from $196 Return on Scoot Airlines. Min 2 Passengers',
-      description: 'Scoot is having a sale on flights to Singapore. Travel in 25/Jul - 15/Nov. Flights are direct to Singapore. Minimum 2 passengers.',
-      url: 'http://iwantthatflight.com.au/d12466-Flights-to-Singapore-from-196-Return-on-Scoot-Airlines-Min-2-Passengers.aspx?afid=2361',
-      image: 'http://iwantthatflight.com.au/pics/locations/SIN_640.jpg'
-    }
-  ];
+cheapSeatApp.controller('DealListController', ['$scope', '$http', function($scope, $http) {
+    $scope.method = 'GET';
+    $scope.url = 'http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361';
+    $scope.code = null;
+    $scope.response = null;
+    $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+        then(function(response) {
+          $scope.status = response.status;
+          $scope.data = response.data;
+        }, function(response) {
+          $scope.data = response.data || 'Request failed';
+          $scope.status = response.status;
+     });
+    $scope.deals = xml2json($scope.data);
 }]);
