a979bceca7746fb68da14e8bd6f56f8d6566c4ff
codz30 <codylgeorge@live.com.au>
2017-07-16 21:51:50 +0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var cheapSeatApp = angular.module('cheapSeatApp', []);
 
 cheapSeatApp.controller('DealListController', ['$scope', '$http', function($scope, $http) {
-    $http.jsonp('http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361', {jsonpCallbackParam: 'callback'})
+    $http.get('https://cors-anywhere.herokuapp.com/http://iwantthatflight.com.au/deals.aspx?format=xml&afid=2361')
     .then(function(response) {
         console.log(response.data);
         console.log(xml2json(response.data));
