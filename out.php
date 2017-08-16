b996b817a7b04b450244379f68b50c6b5da38555
codz30 <codylgeorge@live.com.au>
2017-08-16 16:13:40 +0800

    Delete out.php

--- out.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  var $url = $_GET['u'];
-  if(!$url){
-    return;
-  }
-  else {
-    echo "<html><head><title>Redirecting...</title><body>Redirecting to " + $url + "</body></html>";
-  }
-?>
