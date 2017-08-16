ae3d467bae186a984fbaee27d7befb80dc4fcfe8
codz30 <codylgeorge@live.com.au>
2017-08-16 16:07:08 +0800

    Create out.php

---
+++ out.php
@@ -0,0 +1,9 @@
+<?php 
+  var $url = $_GET['u'];
+  if(!$url){
+    return;
+  }
+  else {
+    echo "<html><head><title>Redirecting...</title><body>Redirecting to " + $url + "</body></html>;
+  }
+?>
